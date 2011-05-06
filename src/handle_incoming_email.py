70c39a77d00d2e0373470a5c6646bd80d55dd80f
marxian <rupert@neontribe.co.uk>
2011-05-06 15:05:41 +0100

    further framework

--- src/handle_incoming_email.py
+++ src/handle_incoming_email.py
@@ -2,20 +2,58 @@ import logging, email
 from google.appengine.ext import webapp 
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler 
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api.urlfetch import fetch
+import urllib
 
-def set_trace():
-    import pdb, sys
-    debugger = pdb.Pdb(stdin=sys.__stdin__, 
-        stdout=sys.__stdout__)
-    debugger.set_trace(sys._getframe().f_back)
+    
+def dbg():
+    """ Enter pdb in App Engine
+    
+    Renable system streams for it.
+    """
+    import pdb
+    import sys
+    pdb.Pdb(stdin=getattr(sys,'__stdin__'),stdout=getattr(sys,'__stderr__')).set_trace(sys._getframe().f_back)
 
-class LogSenderHandler(InboundMailHandler):
+def send_via_kapow():
+    pass
+
+def scrape_ncc(number, street):
+    
+    payload = "__EVENTTARGET=&__EVENTARGUMENT=&__VIEWSTATE=%2FwEPDwUJOTQwMTczNDE4D2QWAmYPZBYCAgIPZBYCAgEPZBYGAgEPZBYEAgEPZBYCAgUPEGRkFgBkAgIPZBYCAgEPEGRkFgBkAgMPFgIeB1Zpc2libGVoZAIFDxYCHwBoZBgBBSpjdGwwMCRDb250ZW50UGxhY2VIb2xkZXIxJGNvbnRlbnRNdWx0aVZpZXcPD2RmZNInlWuoj%2B1qIemQ54M0YaiTyKRa&__EVENTVALIDATION=%2FwEWBAKF%2BrexCwKDh6m8BwLwxu3%2FAQLw2PTaBr%2BMsxUUHGL1U%2BoJyGcNzAk0PBpJ&ctl00%24ContentPlaceHolder1%24streetNameTextBox="
+    payload += urllib.quote_plus(street) 
+    payload += "&ctl00%24ContentPlaceHolder1%24houseNoTextBox="
+    payload += urllib.quote_plus(number)
+    payload += "&ctl00%24ContentPlaceHolder1%24welcomeSearchButton=Search"
+
+
+    response = fetch('http://www.norwich.gov.uk/webapps/awc/bincollectiondayfinder.aspx', payload, 'POST')
+    
+    return response
+
+class IncomingSMSHandler(InboundMailHandler):
     def receive(self, mail_message):
+        dbg()
         plaintext_bodies = mail_message.bodies('text/plain')
+        msg = ' '.join([x[1].decode() for x in plaintext_bodies])
         logging.info("Received a message from: " + mail_message.sender)
         
+        #parse message
+        number = '106'
+        street = 'lincoln'
+        
+        #scrape ncc
+        info = scrape_ncc(number, street)
+        
+        #save result
+        
+        
+        #send response
+        
+        
+        
 #application = webapp.WSGIApplication([('/_ah/mail/sms@binman-norwich.appspotmail.com', LogSenderHandler)], debug=True)
-application = webapp.WSGIApplication([LogSenderHandler.mapping()], debug=True)
+application = webapp.WSGIApplication([IncomingSMSHandler.mapping()], debug=True)
 def main():
     run_wsgi_app(application)
 
